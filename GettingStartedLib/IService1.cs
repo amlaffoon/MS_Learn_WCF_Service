033e680836713b9e92e72441c09e6f785d5aaa3f
Anna Laffoon <ree11194@gmail.com>
2023-12-12 13:36:48 -0500

    implement service contract

--- GettingStartedLib/IService1.cs
+++ GettingStartedLib/IService1.cs
@@ -3,16 +3,39 @@ using System.ServiceModel;
 
 namespace GettingStartedLib
 {
-    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
-    public interface ICalculator
+    public class CalculatorService : ICalculator
     {
-        [OperationContract]
-        double Add(double n1, double n2);
-        [OperationContract]
-        double Subtract(double n1, double n2);
-        [OperationContract]
-        double Multiply(double n1, double n2);
-        [OperationContract]
-        double Divide(double n1, double n2);
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Received Add({0},{1})", n1, n2);
+            // Code added to write output to the console window.
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            Console.WriteLine("Received Divide({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
     }
 }
